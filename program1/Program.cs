6b3a23ab3750dcc2ea4252f3a2208ddb2e7a080d
Veronika <vgsafaryan@gmail.com>
2022-06-30 20:49:56 +0300

    Написали первую программу

--- program1/Program.cs
+++ program1/Program.cs
@@ -1,3 +1,17 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("max = " + a);
+}
+else if (a == b)
+{
+    Console.WriteLine("max = " + a);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+}

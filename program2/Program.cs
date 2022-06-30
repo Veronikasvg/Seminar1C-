30eff526855384796a42152da86071215784c20d
Veronika <vgsafaryan@gmail.com>
2022-06-30 21:45:32 +0300

    Написали вторую программу

---
+++ program2/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Write("Введите число1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("max = " + a);
+    }
+    else if (a == c)
+    {
+        Console.WriteLine("max = " + a);
+    }
+    else
+    {
+        Console.WriteLine("max = " + c);
+    }
+}
+else if (a == b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("max = " + a);
+    }
+    else
+    {
+        Console.WriteLine("max = " + c);
+    }
+}
+else
+{
+    if (a > c)
+    {
+        Console.WriteLine("max = " + b);
+    }
+    else if (b == c)
+    {
+        Console.WriteLine("max = " + b);
+    }
+    else
+    {
+        Console.WriteLine("max = " + c);
+    }
+
+}

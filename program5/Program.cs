4871d727c75413777fac9441a2f769af8c8ebc93
Veronika <vgsafaryan@gmail.com>
2022-07-02 22:54:22 +0300

    Программа показывает вторую цифру

---
+++ program5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = number % 100 / 10;
+
+Console.WriteLine("Вторая цифра заданного числа равна " + number2);
+
+

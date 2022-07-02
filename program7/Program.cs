6fefb2eea9078295b84391c00d87723389de6298
Veronika <vgsafaryan@gmail.com>
2022-07-02 22:58:31 +0300

    Дни недели

---
+++ program7/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите день недели (от 1 до 7): ");
+int Day = Convert.ToInt32(Console.ReadLine());
+
+int saturday = 6,
+    sunday = 7;
+
+if (Day == saturday || Day == sunday)
+{
+    Console.WriteLine("Выходной день");
+}
+else if (Day > 7 || Day <= 0)
+{
+    Console.WriteLine("Необходимо использовать только цифры от 1 до 7");
+}
+else
+{
+    Console.WriteLine("Не выходной день");
+}

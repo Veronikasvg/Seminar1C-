b7ef6bfc9534cd935b4a5622b8240ee02142ab76
Veronika <vgsafaryan@gmail.com>
2022-07-02 22:56:48 +0300

    Выводит третью цифру заданного числа

---
+++ program6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+{
+    Console.Write("Третьей цифры нет");
+}
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    Console.Write("Третья цифра заданного числа равна " + number % 10);
+
+}

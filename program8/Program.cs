963048a5af0a980f7cf99fd52b679e17309bf186
Veronika <vgsafaryan@gmail.com>
2022-07-08 23:01:20 +0300

    Задача на палиндром

---
+++ program8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Палиндром
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine()),
+firstnumber = number / 10000,
+secondnumber = number % 10000 / 1000,
+thirdnumber = number % 100 / 10,
+lastnumber = number % 10;
+if (number >= 10000 && number < 100000)
+{
+    if (firstnumber == lastnumber && secondnumber == thirdnumber)
+    {
+        Console.WriteLine("Палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Неверное число");
+}

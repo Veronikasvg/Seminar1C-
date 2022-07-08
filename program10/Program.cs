6351638a34982c2a1580a973a73f6c179eaeb49e
Veronika <vgsafaryan@gmail.com>
2022-07-08 23:04:04 +0300

    Кубы

---
+++ program10/Program.cs
@@ -0,0 +1,17 @@
+﻿// принимает на вход число и выдает таблицу кубов
+Console.Write($"Введите число от 1 до {int.MaxValue}: ");
+int userNumber = Convert.ToInt32(Console.ReadLine()),
+count = 1;
+string result = "";
+
+while (count <= userNumber)
+{
+    result += count * count * count;
+    if (count != userNumber)
+    {
+    result += ", "; 
+    }
+    count++;
+}
+
+Console.WriteLine(result);

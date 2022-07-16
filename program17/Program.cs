2a3ca51995370ba0b4681ff8788f89ccd24bf90b
Veronika <vgsafaryan@gmail.com>
2022-07-16 21:44:55 +0300

    Больше нуля

---
+++ program17/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine(" Введите количество элементов массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[m];
+int positiveNumber = 0;
+FillArray(numbers);
+PrintArray(numbers);
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        positiveNumber++;
+    }
+
+}
+Console.WriteLine($"Количество чисел больше нуля: {positiveNumber}");
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}

e47673a515c0fd711ba9cb1e5b4cde3c1c1d6a27
Veronika <vgsafaryan@gmail.com>
2022-07-29 23:16:47 +0300

    Задача вывод суммы

---
+++ program27/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int GetNumber(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int PrintNumber(int number1, int number2)
+{
+    int max = number1 > number2 ? number1 : number2,
+        min = number2 < number1 ? number2 : number1;
+    if (min != max)
+        return PrintNumber(max - 1, min) + max;
+    return min;
+}
+int number1 = GetNumber("Введите число №1: "),
+    number2 = GetNumber("Введите число №2: ");
+
+Console.Write(PrintNumber(number1, number2));
